ee9e81f7034d4af74a6ae8bcc41bdab74f339406
Alejandro Gilson <alex.gilsoncampillo@gmail.com>
2022-01-30 13:44:03 +0100

    Changed directory to PitchEngine

--- src/PitchEngineConfig.h
+++ src/PitchEngineConfig.h
@@ -1,10 +1,10 @@
 #ifndef DSP_CONFIG_H
 #define DSP_CONFIG_H
 
-#define DATA_DIR "C:/Users/alexg/source/repos/DSPSim/data//"
-#define INPUT_AUDIO_DIR "C:/Users/alexg/source/repos/DSPSim/data/inputAudio//"
-#define OUTPUT_AUDIO_DIR "C:/Users/alexg/source/repos/DSPSim/data/outputAudio//"
-#define DEBUG_DIR "C:/Users/alexg/source/repos/DSPSim/data/debugData//"
+#define DATA_DIR "C:/Users/alexg/source/repos/PitchEngine/data//"
+#define INPUT_AUDIO_DIR "C:/Users/alexg/source/repos/PitchEngine/data/inputAudio//"
+#define OUTPUT_AUDIO_DIR "C:/Users/alexg/source/repos/PitchEngine/data/outputAudio//"
+#define DEBUG_DIR "C:/Users/alexg/source/repos/PitchEngine/data/debugData//"
 
 #ifdef USE_DOUBLE
 typedef double my_float;
