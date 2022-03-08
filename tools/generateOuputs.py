7fac6835a27a43a2f064873bbec8284d39b9651b
Alejandro Gilson <alex.gilsoncampillo@gmail.com>
2022-03-08 18:35:32 +0100

    Deleted generateOutputs.py

--- tools/generateOuputs.py
+++
@@ -1,9 +0,0 @@
-
-import os
-import numpy as np
-
-count = 0
-for i in np.linspace(0, 2, 100):
-    count = count + 1
-    print(count)
-    os.system("./bin/WSL-GCC-Debug/DSPSim sine_short.wav output_" + str(i).replace(".", "_") + ".wav " + str(i))
